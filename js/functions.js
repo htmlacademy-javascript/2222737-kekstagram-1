6b6763ecd0b6974cef2a05a9034fe69387fd3838
Antonio-Schultz <lebedeanton@gmail.com>
2024-02-01 20:25:37 +0300

    getString function is added, that composes an output string from an input string and additional characters

--- js/functions.js
+++ js/functions.js
@@ -37,3 +37,35 @@ let extractDigits = function (string) {
   }
 };
 
+// Функция, добавляющая символы из заданного набора
+//к входящей строке до указанной длины
+
+function getString (incString, stringLength, addSymbols) {
+  const inputLength = incString.length;
+  const addLength = addSymbols.length;
+  if (inputLength >= stringLength) {
+    return incString;
+  } else {
+    //'дуб', 7, 'бук' difference = 4, quantity = 1, leftover = 1
+    const difference = stringLength - inputLength;
+    const quantity = Math.floor(difference / addLength);
+    const leftover = difference % addLength;
+    let combinedString1 = '';
+    for (let i = 0; i < quantity; i++) {
+      combinedString1 += addSymbols;
+    }
+    let currentIndex = 0;
+    let combinedString2 = '';
+    for (let j = 0; j < leftover; j++){
+      combinedString2 += addSymbols[currentIndex];
+      if (addSymbols[currentIndex + 1]) {
+        currentIndex += 1;
+      } else {
+        currentIndex = 0;
+      }
+    }
+    const totalString = combinedString1 + combinedString2 + incString;
+    return totalString;
+  }
+}
+
