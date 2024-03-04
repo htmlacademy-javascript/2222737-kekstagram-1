2fdf5da0b9d54639f4f60b804d575e18d6afaa81
Antonio-Schultz <lebedeanton@gmail.com>
2024-03-04 20:18:31 +0300

    Module4-task1 is finished

--- js/main.js
+++ js/main.js
@@ -1,47 +1,59 @@
-let names = ['Александр', 'Леонид', 'Вячеслав', 'Сергей', 'Влад', 'Петр'];
+const NAMES = ['Александр', 'Леонид', 'Вячеслав', 'Сергей', 'Влад', 'Петр'];
 
-let messages = ['Все отлично!', 'В целом, всё неплохо. Но не всё.', 'Когда вы делаете фотографии, хорошо бы убирать палец из кадра. В конце концов, это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const MESSAGES = ['Все отлично!', 'В целом, всё неплохо. Но не всё.', 'Когда вы делаете фотографии, хорошо бы убирать палец из кадра. В конце концов, это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
+const NUMBER_OF_OBJECTS = 25;
 
 let getRandomInteger = (min, max) => {
   let minValue = Math.floor(Math.min(Math.abs(min), Math.abs(max)));
   let maxValue = Math.ceil(Math.max(Math.abs(min), Math.abs(max)));
-  let randomValue = Math.random * (maxValue - minValue + 1) + minValue;
+  let randomValue = Math.random() * (maxValue - minValue + 1) + minValue;
   return Math.floor(randomValue);
 };
 
 let getRandomElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-let generateId = () => {
-  let previousIds = [];
+let generateUniqueRandomValues = (x,y) => {
+  let previousValues = [];
   return function () {
-    let currentId = getRandomInteger(1, 25);
-    while (previousIds.includes(currentId)) {
-      currentId = getRandomInteger(1, 25);
-    };
-    previousIds.push(currentId);
-    return currentId;
+    let currentValue = getRandomInteger(x, y);
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(x, y);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
   };
 };
 
-let generateUrl = () => {
-  let previousUrls = [];
+let generateRandomValues = () => {
+  let previousValues = [];
   return function () {
-    let currentUrl = getRandomInteger(1, 25);
-    while (previousUrls.includes(currentUrl)) {
-      currentUrl = getRandomInteger(1, 25);
-    };
-    previousUrls.push(currentUrl);
-    return currentUrl;
+    let currentValue = Math.random() * 1000;
+    while (previousValues.includes(currentValue)) {
+      currentValue = Math.random() * 1000;
+    }
+    previousValues.push(currentValue);
+    return currentValue;
   };
 };
+
+
+let generateComment = () => ({
+  id: generateRandomValues(),
+  avatar: `/img/avatar-${getRandomInteger(1, 6)}`,
+  message: getRandomElement(MESSAGES),
+  name: getRandomElement(NAMES),
+});
 
 
 let generateObject = () =>
-{
-  id: '',
-  url: '',
-  description: 'Вот такое вот фото',
-  likes: '',
-  comments: '',
-};
+  ({
+    id: generateUniqueRandomValues(1, 25),
+    url: `photos/${generateUniqueRandomValues(1, 25)}.jpg`,
+    description: 'Вот такое вот фото',
+    likes: getRandomInteger(15, 200),
+    comments: generateComment(),
+  });
+
+let arrayOfObjects = Array.from({length: NUMBER_OF_OBJECTS}, generateObject);
+
